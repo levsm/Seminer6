5669d6f4aab6301a8d0dad417c154dffa4d5323b
Lev <levsmirnov@mail.ru>
2022-08-29 10:49:22 +0300

    task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,33 +3,18 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите колличество элементов массива:");
-bool M = Convert.ToInt32(Console.ReadLine());
-bool[] array = new bool[M];
-bool sumP=0;
-for ( i = 0; i < array.Length; i++)
-{
-    array[i] = sumP>0;
-    sumP = array[i];
-}
-Console.WriteLine($"Больше нуля = {sumP}");
 
+
+Console.Write("Введите элементы(через запятую): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
  
-// Console.WriteLine("Введите колличество элементов массива:");
-// int A = Convert.ToInt32(Console.ReadLine());
-// int[] array = new int[A];
-// Random rand = new Random();
-
-// for (int i = 0; i < array.Length; i++)
-// {
-//     array[i] = rand.Next(-99, 100);
-// }
-// Console.WriteLine(string.Join(", ", array));
-
-// for (int i = 0; i < array.Length; i++)
-// {
-//     //array[i] = array[i] * -1;
-//     array[i] *= -1;
-//     //array[i] = - array[i];
-// }
-// Console.WriteLine(string.Join(", ", array));
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
